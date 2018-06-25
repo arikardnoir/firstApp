e506db78944d6409af98ac55f2f8854f625e4069
Aristoteles Lopes <aristoteleslopes@MacBook-Pro-de-Aristoteles.local>
2018-06-25 14:17:33 -0300

    First Commit

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+
+  selector: 'nav-bar',
+  templateUrl: './navbar/navbar.component.html'
+
+})
+export class NavBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
